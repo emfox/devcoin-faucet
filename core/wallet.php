19c692e8eec21684b1eb2c3fc03dd7498f319064
Emfox Zhou <emfoxzhou@gmail.com>
2025-03-26 03:22:13 -0400

    Remove unused code.

--- core/wallet.php
+++ core/wallet.php
@@ -6,18 +6,10 @@ $start = timer();
 
 include("config.php");
 include_once ("includes/jsonRPCClient.php");
-include("address.inc");
-include("recaptchalib.inc");
-include ("adscaptchalib.inc");
-
-//captha
-$publickey = "6LfYSssSAAAAAF2w_TeMklmv-6VWUDhcECr9rWfI";
-$privatekey = "6LfYSssSAAAAAPntQz9H0twbsdyk8kQHO_F4mupD";
 
 // init
 
 $btclient = new jsonRPCClient("http://". $btclogin["username"] . ':' . $btclogin["password"] . '@' .$btclogin["host"] . ':' . $btclogin["port"]);
-$addr = new Address($btclient,$sqlogin);
 $derp = $btclient->getblockchaininfo();
 
 //$this->PDO_Conn = new PDO("mysql:host={$sqllogin['host']};dbname={$sqllogin['dbname']}", $sqllogin['username'], $sqllogin['password']);
